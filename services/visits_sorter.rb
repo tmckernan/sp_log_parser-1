9fc5fc683585dd5b69772662ff369607c58041eb
Nick Vyrko <nikita.vyrko@gmail.com>
2020-02-09 15:06:11 +0300

    VisitsSorter implementation

--- services/visits_sorter.rb
+++ services/visits_sorter.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
+require_relative '../constants/sorter_constants'
+
 # Sorts the endpoints visits by visits
 class VisitsSorter
-  def self.sort(endpoints_visits, direction: SorterConstants::DESC); end
+  def self.sort(endpoints_visits, direction: SorterConstants::DESC)
+    if direction == SorterConstants::DESC
+      descending_sort(endpoints_visits)
+    elsif direction == SorterConstants::ASC
+      ascending_sort(endpoints_visits)
+    end
+  end
+
+  def self.descending_sort(endpoints_visits)
+    endpoints_visits.sort { |a, b| b.visits <=> a.visits }
+  end
+
+  def self.ascending_sort(endpoints_visits)
+    endpoints_visits.sort { |a, b| a.visits <=> b.visits }
+  end
 end
