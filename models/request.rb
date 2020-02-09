3c572a8fd6d26ba6c2bfa70c80265fbfad4e780f
Nick Vyrko <nikita.vyrko@gmail.com>
2020-02-09 13:50:33 +0300

    Request model implementation
    - Add rubocop rule to allow '//' for regex

--- models/request.rb
+++ models/request.rb
@@ -3,9 +3,16 @@
 # A model that represents single request from logs
 # @param log_line [String] single string from logfile
 class Request
+  ENDPOINT_REGEX = /^\/\w+(\/\w+)*$/.freeze
+  IP_REGEX = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.freeze
+
+  attr_reader :endpoint, :ip
+
   def initialize(log_line)
     @endpoint, @ip = log_line.split(' ')
   end
 
-  def validate!; end
+  def validate!
+    raise CorruptedDataError if @endpoint !~ ENDPOINT_REGEX || @ip !~ IP_REGEX
+  end
 end
